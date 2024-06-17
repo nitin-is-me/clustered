bec4b3b3b97343a8a44a119d8e5abbf53394c3be
Nitin <nitinjha2609@gmail.com>
2024-06-17 22:58:59 +0530

    finished

---
+++ README.md
@@ -0,0 +1,17 @@
+# Clustered
+Clustered is a notes and ideas writing platform, but with a catch:
+<br>
+The twist is, everyone's notes will be saved at the same place, irrespective of any device. Anyone using this platform can see your notes, and vice versa.
+<br>
+You can't delete the notes yourself, it's under my control (I thought that's an interesting feature to include).
+<br>
+You have to enter the note with your name (not necessarily your real name), so that it can be identified
+<br>
+You can visit the app <a href="https://nitin-is-me.github.io/clustered/">here</a>
+
+## How it's made
+You can check the front end code in this repo, HTML and Javascript is used along with CSS and Bootstrap.
+<br>
+About backend, I'm hosting the server on Vercel, so you can use the platform 24/7.
+<br>
+Still if you want to check the backend code, it's available <a href="https://www.github.com/nitin-is-me/clustered-backend">here</a>.
---
+++ script.js
@@ -0,0 +1,38 @@
+document.getElementById("saveNote").addEventListener("click", async()=>{
+    const content=document.getElementById("noteContent").value;
+    const name = document.getElementById("noteName").value;
+    if (content && name){
+        try {
+            await axios.post("https://clustered-one.vercel.app/api/notes", {content, name});
+            document.getElementById("noteContent").value="";
+            document.getElementById("noteName").value="";
+            loadNotes();
+        } catch(err){
+            console.log(err)
+        }
+    }
+    else {
+        alert("Please enter both note and your name");
+    }
+});
+
+async function loadNotes(){
+    try {
+        const response = await axios.get("https://clustered-one.vercel.app/api/notes");
+        const notesList = document.getElementById("notesList");
+
+        notesList.innerHTML="";
+        response.data.forEach(note => {
+            const li = document.createElement("li");
+            li.className="list-group-item mt-3";
+            li.innerHTML=`<p>${note.content}</p>
+            <small>By ${note.name} at ${new Date(note.createdAt).toLocaleString()}</small>`;
+
+            notesList.appendChild(li);
+        })
+    } catch(err){
+        console.log("Error loading notes:", err);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadNotes);
